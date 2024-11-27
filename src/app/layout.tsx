5f13fff719f99738c93931dbd10f613d001b1171
GyeongChan Jang <cham9994@gmail.com>
2024-11-27 15:05:52 +0900

    feat: open graph 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,39 @@ import { cn } from '@/lib/utils'
 
 import '@/styles/globals.css'
 import { Toaster } from '@/components/ui/toaster'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  // title: 'Run Daily',
+  title: '런데일리',
+  // description: 'Visualize your running records like GitHub contribution graph',
+  description: '러닝 잔디밭을 확인하세요 🌱',
+  openGraph: {
+    // title: 'Run Daily',
+    title: '런데일리',
+    // description: 'Visualize your running records like GitHub contribution graph',
+    description: '러닝 잔디밭을 확인하세요 🌱',
+    url: 'https://rundaily.vercel.app',
+    siteName: '런데일리',
+    images: [
+      {
+        url: 'https://rundaily.vercel.app/og-image.png', // 1200x630px 권장
+        width: 1200,
+        height: 630,
+        alt: '런데일리 미리보기'
+      }
+    ],
+    locale: 'en_US',
+    type: 'website'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '런데일리',
+    // description: 'Visualize your running records like GitHub contribution graph',
+    description: '러닝 잔디밭을 확인하세요 🌱',
+    images: ['https://rundaily.vercel.app/og-image.png']
+  }
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient({
